a34a9b38bbb4096f22518f30584649ca247542f0
shams342 <shams.kadri@ryerson.ca>
2022-09-05 03:58:06 -0400

    mapped the card components after learning array.map and props

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,35 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import dataElements from './data'
 
 function App() {
+
+  const data = dataElements.map(properties => {
+    return <Card 
+    
+      img={properties.coverImg} 
+      rating={properties.stats.rating}
+      reviewCount={properties.stats.reviewCount}
+      country={properties.location}
+      title={properties.title}
+      price={properties.price}/> 
+  })
+
   return (
     <div className='container'>
       <Navbar/>
       <Hero/>
-      <Card/>
+      {data}
+      
+      {/* <Card
+        img="katie-zaferes.png"
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price="136"  
+      /> */}
     </div>    
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
-import image from '../images/katie-zaferes.png'
-import star from '../images/star.png'
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className="card">
-            <img src={image} className="card--image" />
+            <img src={`./src/images/${props.img}`} className="card--image" />
             <div className="card--stats">
-                <img src={star} className='card--star'/>
-                <span>5.0</span>
-                <span className='gray'>(6) • </span>
-                <span className='gray'>USA</span>
+                <img src="./src/images/star.png" className='card--star'/>
+                <span>{props.rating}</span>
+                <span className='gray'>({props.reviewCount}) •{'\u00a0'}</span>
+                <span className='gray'>{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className='bold'>From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className='bold'>From ${props.price}</span> / person</p>
         </div>
     )
-
-    
 }
