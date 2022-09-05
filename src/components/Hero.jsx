4f7535ab279f283d6a068604e39618c012e47a99
shams342 <shams.kadri@ryerson.ca>
2022-09-05 05:31:53 -0400

    moved images folder to public folder

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Hero() {
     return (
         <section className='hero'>
-            <img src="./src/images/photo-grid.png" className="hero--image"/>
+            <img src="../../images/photo-grid.png" className="hero--image"/>
             <h1>Online Experiences</h1>
             <p className='description'>Join unique interactive activities led by one-of-a-kind 
                 hosts-all without leaving home.
